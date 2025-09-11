14c4e51e5f8be94b3d4a7e1cf96dce02c60fa4d9
Anushka Verma <anushkaverma779@gmail.com>
2025-09-11 22:08:13 +0530

    basic style change

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ const App = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        flexDirection: "column",
+        gap: "20px",
       }}
     >
       <h1>Anushka Verma</h1>
-      <br />
-      <br />
       <h4>Re-developing this portfolio website</h4>
     </div>
   );
